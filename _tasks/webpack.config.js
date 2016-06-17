8edf297919992933ceed6363e6abdf96d68189d7
Aaron <ly1992@qq.com>
2016-06-17 15:46:06 +0800

    'save'

--- _tasks/webpack.config.js
+++ _tasks/webpack.config.js
@@ -1,22 +1,26 @@
-module.exports = function (outputPath,type) {
+var path = require('path');
+
+
+console.log(path.resolve('../' , __dirname));
+console.log(path.resolve('../' , __dirname, 'public'));
+
+module.exports = function (outputPath, type) {
     return {
         output: {
-            publicPath: type == 'dev'?'js/page/' + outputPath + '/dist/':'js/' + outputPath + '/',
+            publicPath: type == 'dev' ? 'js/page/' + outputPath + '/dist/' : 'js/' + outputPath + '/',
             filename: 'index.js',
         },
         module: {
-            loaders:[
-
-                {
-                    test: /\.css$/,
-                    loader: 'style-loader!css-loader?modules'
-                }
+            loaders: [
+                {test: /\.css$/, loader: "style-loader!css-loader"},
+                {test: /\.png$/, loader: "url-loader?limit=100000"},
+                {test: /\.jpg$/, loader: "file-loader"}
             ]
         },
         resolve: {
-            root: './public/',
-            alias:{
-                'jquery':'lib/jquery/jquery'
+            root: ['./public/css/','./public/'],
+            alias: {
+                'jquery': 'lib/jquery/jquery'
             }
         },
     }
